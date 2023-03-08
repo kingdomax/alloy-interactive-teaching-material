9396231006066e76e05584a024462f5886430348
kingdomax <kingdomax@windowslive.com>
2023-03-08 01:24:48 +0100

    Refactor walkThroughTheLesson()

--- app/src/main/java/alloy/interactive/teaching/material/App.java
+++ app/src/main/java/alloy/interactive/teaching/material/App.java
@@ -6,6 +6,9 @@ import alloy.interactive.teaching.material.validator.CommandLineValidator;
 import alloy.interactive.teaching.material.validator.alloy.AlloyValidator;
 
 public class App {
+    interface Condition { boolean test(String input); }
+    interface Part { String execute(String message, String partName, boolean useLessonNumberFromInput, String lessonNumber, Condition condition); }
+    
     public static void main(String[] args) {
         if (args.length > 0) { jumpToThePoint(args); }
         else { walkThroughTheLesson(); }
@@ -17,41 +20,23 @@ public class App {
         else { FileHelper.readFile(args[1], args[3], true, false); }
     }
 
-    // todo-moch: re-write to be more cleaner
-    private static void walkThroughTheLesson() {
-        // welcome message
-        FileHelper.readFile("", "welcome", true, false);
+    private static void walkThroughTheLesson() {     
+        Part part = (message, partName, useLessonNumberFromInput, lessonNumber, condition) -> {
+            String input = "";
+            while (condition.test(input)) {
+                ConsoleHelper.response(message, true, -1);
+                input = System.console().readLine().toLowerCase();
+            }
+            
+            lessonNumber = useLessonNumberFromInput ? input : lessonNumber;
+            FileHelper.readFile(lessonNumber, partName, true, true);
+            return lessonNumber;
+        };
         
-        // explanation
-        String lesson = "";
-        while (!lesson.equals("1") && !lesson.equals("2") && !lesson.equals("3")) { // todo-moch: remove this hardcode so it's flexible to add new lesson
-            ConsoleHelper.response("What lesson are you interested in learning today? (1-3): ", true, -1);
-            lesson = System.console().readLine().toLowerCase();
-        }
-        FileHelper.readFile(lesson, "explanation", true, true);
-
-        // example
-        String ans = "";
-        while (!ans.equals("y")) {
-            ConsoleHelper.response("Would you like to see syntax example? (y/n): ", true, -1);
-            ans = System.console().readLine().toLowerCase();
-        }
-        FileHelper.readFile(lesson, "example", true, true);
-
-        // exercise
-        ans = "";
-        while (!ans.equals("y")) {
-            ConsoleHelper.response("It's time for an exercise. Shall we start? (y/n): ", true, -1);
-            ans = System.console().readLine().toLowerCase();
-        }
-        FileHelper.readFile(lesson, "exercise", true, true);
-
-        // farewell message
-        ans = "";
-        while (!ans.equals("y")) {
-            ConsoleHelper.response("The exercise can be completed at your own pace, saved in a separate file, and submitted to our app when completed. Let's proceed for now. (y/n): ", true, -1);
-            ans = System.console().readLine().toLowerCase();
-        }
-        FileHelper.readFile("", "farewell", true, false);
+        FileHelper.readFile("", "welcome", true, false);
+        String lesson = part.execute("What lesson are you interested in learning today? (1-3): ", "explanation", true, "", input -> !input.equals("1") && !input.equals("2") && !input.equals("3"));
+        part.execute("Would you like to see syntax example? (y/n): ", "example", false, lesson, input -> !input.equals("y"));
+        part.execute("It's time for an exercise. Shall we start? (y/n): ", "exercise", false, lesson, input -> !input.equals("y"));
+        part.execute("The exercise can be completed at your own pace, saved in a separate file, and submitted to our app when completed. Let's proceed for now. (y/n): ", "farewell", false, "", input -> !input.equals("y"));
     }
 }
