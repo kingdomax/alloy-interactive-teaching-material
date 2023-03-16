748f76fe00ba267cb41b7868f135504db3f3864a
kingdomax <kingdomax@windowslive.com>
2023-03-16 22:29:33 +0100

    Use ClassLoader instead of getAbsolutePath()

--- app/src/main/java/alloy/interactive/teaching/material/helper/FileHelper.java
+++ app/src/main/java/alloy/interactive/teaching/material/helper/FileHelper.java
@@ -1,15 +1,14 @@
 package alloy.interactive.teaching.material.helper;
 
 import java.io.File;
-import java.io.FileReader;
-import java.nio.file.Paths;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.InputStreamReader;
 
 public class FileHelper {
-    private static final String RESOURCES_PATH = "app/bin/main/";
+    private static final String RESOURCES_PATH = "";
     private static final String WELCOME_MSG = "welcome.txt";
     private static final String FAREWELL_MSG = "farewell.txt";
     private static final String ANALYZER_MSG = "analyzer.txt";
@@ -20,14 +19,15 @@ public class FileHelper {
     private static final String LINE = "################################################################################################";
 
     public static void readFile(String lesson, String file, boolean printLine, boolean copyFile) {
-        BufferedReader reader = null;
-        var fullPath = getAbsolutePath(lesson, file);
+        InputStream inputStream = null;
+        var fullPath = RESOURCES_PATH + (lesson.equals("") ? "" : lesson + "/") + getFileName(file);
         
         try {
             System.out.println(printLine ? LINE : "");
+            inputStream = FileHelper.class.getClassLoader().getResourceAsStream(fullPath);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
             
             String line;
-            reader = new BufferedReader(new FileReader(fullPath));
             while ((line = reader.readLine()) != null) { System.out.println(line); }
             if (copyFile) { copyFile(lesson, file, fullPath); }
             
@@ -35,25 +35,25 @@ public class FileHelper {
         } catch (IOException e) { 
             e.printStackTrace();
         } finally {
-            try { reader.close(); } 
-            catch (IOException e) { e.printStackTrace(); }
+            try {
+                if (inputStream != null) { inputStream.close(); }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     private static void copyFile(String lesson, String file, String sourcePath) {
         var outputFileName = lesson + "_" + getFileName(file);
         if (new File(outputFileName).exists()) { return; }
-        
-        try {
-            FileInputStream inputStream = new FileInputStream(sourcePath);
-            FileOutputStream outputStream = new FileOutputStream(outputFileName);
+    
+        try (InputStream inputStream = FileHelper.class.getClassLoader().getResourceAsStream(sourcePath)) {
+            if (inputStream == null) { throw new IOException("Resource not found: " + sourcePath); }
 
-            // Read from the input stream and write to the output stream until the end of the file is reached
+            FileOutputStream outputStream = new FileOutputStream(outputFileName);
             byte[] buffer = new byte[1024]; // Create a byte array to hold the data read from the input stream
             int bytesRead;
-            while ((bytesRead = inputStream.read(buffer)) > 0) { outputStream.write(buffer, 0, bytesRead); }
-
-            inputStream.close();
+            while ((bytesRead = inputStream.read(buffer)) > 0) { outputStream.write(buffer, 0, bytesRead); } // Read from the input stream and write to the output stream until the end of the file is reached
             outputStream.close();
         } catch (IOException e) {
             e.printStackTrace();
@@ -71,10 +71,5 @@ public class FileHelper {
             case "exercise-solution":   return SOLUTION_FILE;
         }
         return "";
-    }
-
-    private static String getAbsolutePath(String lesson, String file) {
-        var filePath = RESOURCES_PATH + (lesson.equals("") ? "" : lesson + "/") + getFileName(file);
-        return Paths.get(filePath).toAbsolutePath().toString();
     }
 }
